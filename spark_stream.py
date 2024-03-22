b95d11d67db19c96894156846be4d71b9d927023
ZeroTwoData <rlworley4data@gmail.com>
2024-03-22 13:23:14 -0400

    update .jars and .py file

--- spark_stream.py
+++ spark_stream.py
@@ -69,8 +69,8 @@ def create_spark_connection():
     try:
         s_conn = SparkSession.builder \
             .appName('SparkDataStreaming') \
-            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.13:3.5.0,"
-                                           "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0") \
+            .config('spark.jars.packages', "com.datastax.spark:spark-cassandra-connector_2.12:3.4.0,"
+                                           "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0") \
             .config('spark.cassandra.connection.host', 'localhost') \
             .getOrCreate()
 
